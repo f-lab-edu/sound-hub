0b5cdfb83b95f984df93655ed2bf063734737626
dhchoi98 <chleogks9808@naver.com>
2024-04-24 22:34:42 +0900

    [FEAT] #7 : UserControllerTest 에 login 테스트 추가

--- src/main/java/com/example/soundhub/infrastructure/mapper/UserMapper.java
+++ src/main/java/com/example/soundhub/infrastructure/mapper/UserMapper.java
@@ -1,22 +1,23 @@
 package com.example.soundhub.infrastructure.mapper;
 
-import com.example.soundhub.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import com.example.soundhub.domain.User;
+
 @Mapper
 @Repository
 public interface UserMapper {
-    @Select("SELECT * FROM users WHERE id = #{id}")
-    User findUserById(Long id);
+	@Select("SELECT * FROM users WHERE id = #{id}")
+	User findUserById(Long id);
 
-    @Select("SELECT * FROM users WHERE login_id = #{loginId}")
-    User findUserByLoginId(String loginId);
+	@Select("SELECT * FROM users WHERE login_id = #{loginId}")
+	User findUserByLoginId(String loginId);
 
-    int create(User user);
+	int create(User user);
 
-    @Select("SELECT EXISTS(SELECT 1 FROM users WHERE login_id = #{loginId})")
-    boolean existsByLoginId(String loginId);
+	@Select("SELECT EXISTS(SELECT 1 FROM users WHERE login_id = #{loginId})")
+	boolean existsByLoginId(String loginId);
 }
 
