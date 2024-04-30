a465740ae90348b90726762600988740344b65bc
DaehanChoi <104803789+dhchoi98@users.noreply.github.com>
2024-04-30 12:36:39 +0900

    [#12] 이미지 업로드 기능 구현 (#13)
    * [FEAT] #12 : 의존성 및 yml 파일에 S3 추가 (.gitignore 수정)
    
    * [FEAT] #12 : S3Config 클래스 추가
    
    * [FEAT] #12 : S3Service 클래스 추가
    
    * [FEAT] #12 : WorkController 이미지 테스트 메소드 추가
    
    * [FIX] #12 : S3Service 수정 (메소드 수정, 로컬 폴더에 파일 저장 되는 오류 해결)
    
    * [FEAT] #12 : S3Service 클래스에 이미지 삭제 메소드 추가
    
    * [FEAT] #12 : S3 LocalStack 테스트 추가

---
+++ src/main/java/com/example/soundhub/application/service/S3Service.java
@@ -0,0 +1,103 @@
+package com.example.soundhub.application.service;
+
+import static com.example.soundhub.config.exception.ErrorResponseStatus.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.soundhub.config.exception.DatabaseException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+	private final AmazonS3 amazonS3Client;
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+		log.debug("Attempting to upload file [{}] to directory [{}]", multipartFile.getOriginalFilename(), dirName);
+
+		File uploadFile = convert(multipartFile)
+			.orElseThrow(() -> new IllegalArgumentException("Failed to convert MultipartFile to File"));
+
+		try {
+			String fileUrl = uploadToS3(uploadFile, dirName);
+			if (!uploadFile.delete()) {
+				log.error("Failed to delete temporary file [{}]", uploadFile.getPath());
+			}
+			return fileUrl;
+		} catch (Exception e) {
+			log.error("Error uploading file [{}] to S3 bucket [{}]", uploadFile.getName(), bucket, e);
+			throw e;
+		}
+	}
+
+	private String uploadToS3(File uploadFile, String dirName) {
+		String fileName = dirName + "/" + uploadFile.getName();
+		log.debug("Uploading file [{}] to S3 bucket [{}]", fileName, bucket);
+		amazonS3Client.putObject(
+			new PutObjectRequest(bucket, fileName, uploadFile)
+				.withCannedAcl(CannedAccessControlList.PublicRead)
+		);
+		String fileUrl = amazonS3Client.getUrl(bucket, fileName).toString();
+		log.info("File [{}] uploaded successfully to URL [{}]", fileName, fileUrl);
+		return fileUrl;
+	}
+
+	private Optional<File> convert(MultipartFile file) throws IOException {
+		File convertFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
+		log.debug("Creating temporary file [{}] for conversion", convertFile.getPath());
+
+		if (convertFile.createNewFile()) {
+			try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+				fos.write(file.getBytes());
+				log.info("Temporary file [{}] created successfully", convertFile.getPath());
+				return Optional.of(convertFile);
+			} catch (IOException e) {
+				log.error("Failed to write to temporary file [{}]", convertFile.getPath(), e);
+				throw e;
+			}
+		} else {
+			log.error("Failed to create new file [{}]", convertFile.getPath());
+			return Optional.empty();
+		}
+	}
+
+	public void deleteImageFromS3(String imageAddress) {
+		String key = getKeyFromImageAddress(imageAddress);
+		try {
+			amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, key));
+		} catch (Exception e) {
+			throw new DatabaseException(IMAGE_DELETE_ERROR);
+		}
+	}
+
+	private String getKeyFromImageAddress(String imageAddress) {
+		try {
+			URL url = new URL(imageAddress);
+			String decodingKey = URLDecoder.decode(url.getPath(), "UTF-8");
+			return decodingKey.substring(1); // 맨 앞의 '/' 제거
+		} catch (MalformedURLException | UnsupportedEncodingException e) {
+			throw new DatabaseException(IMAGE_DELETE_ERROR);
+		}
+	}
+}
