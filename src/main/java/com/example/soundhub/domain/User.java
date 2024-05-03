e1378af88f897091e9346d30db6bd67f7ae0f355
dhchoi98 <chleogks9808@naver.com>
2024-05-03 16:16:47 +0900

    [REFACTOR] #12 : 테스트를 위한 유저 쪽 코드 리팩토링, 스키마 파일에 works 테이블 추가

--- src/main/java/com/example/soundhub/domain/User.java
+++ src/main/java/com/example/soundhub/domain/User.java
@@ -1,24 +1,25 @@
 package com.example.soundhub.domain;
 
-
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-
 @EqualsAndHashCode
 @Getter
 public class User {
-    private final String name;
-    private final String loginId;
-    private final String password;
-    private Long id;
+	private final String name;
+	private final String loginId;
+	private final String password;
+	private Long id;
 
+	@Builder
+	public User(String name, String loginId, String password) {
+		this.name = name;
+		this.loginId = loginId;
+		this.password = password;
+	}
 
-    @Builder
-    public User(String name, String loginId, String password) {
-        this.name = name;
-        this.loginId = loginId;
-        this.password = password;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
