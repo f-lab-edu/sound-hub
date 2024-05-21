f698a2be0a3e4c640fe0c2dba361bb18c6cb2508
dhchoi98 <chleogks9808@naver.com>
2024-05-21 10:05:48 +0900

    [REFACTOR] #12 : application-private.yml 삭제

--- src/main/java/com/example/soundhub/presentation/controller/WorkController.java
+++ src/main/java/com/example/soundhub/presentation/controller/WorkController.java
@@ -1,14 +1,7 @@
 package com.example.soundhub.presentation.controller;
 
-import java.io.IOException;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.example.soundhub.application.service.S3Service;
 
@@ -21,22 +14,4 @@ public class WorkController {
 
 	private final S3Service s3Service;
 
-	@GetMapping("/tokenTest")
-	public ResponseEntity<String> test() {
-		return ResponseEntity.ok("good");
-	}
-
-	@PostMapping("/s3/upload")
-	public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image) {
-		String profileImage = "";
-		if (image != null) {
-			try {// 파일 업로드
-				profileImage = s3Service.upload(image, "images");
-				System.out.println("fileURL = " + profileImage);
-			} catch (IOException e) {
-
-			}
-		}
-		return ResponseEntity.ok(profileImage);
-	}
 }
